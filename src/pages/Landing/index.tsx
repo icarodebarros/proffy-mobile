34b77ddfe0b14b750acf252b4fd1e9717158a9d2
Icaro de Barros <icaro.barros@banksystem.com.br>
2020-08-19 09:57:58 -0300

    Página Landing consumindo API via axios

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Image, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
+
+import api from '../../services/api';
 
 import styles from './styles';
 
@@ -12,6 +14,16 @@ import heartIcon from '../../assets/images/icons/heart.png';
 
 function Landing() {
     const { navigate } = useNavigation();
+    const [ totalConnections, setTotalconnections ] = useState(0);
+
+    useEffect(() => {
+        api.get('connections')
+            .then(res => {
+                const { total } = res.data;
+
+                setTotalconnections(total);
+            });
+    }, []);
 
     function handleNavigateToGiveClassesPage() {
         navigate('GiveClasses');
@@ -43,7 +55,7 @@ function Landing() {
             </View>
 
             <Text style={styles.totalConnections}>
-                Total de 286 conexões já realizadas {' '}
+                Total de {totalConnections} conexões já realizadas {' '}
                 <Image source={heartIcon} />
             </Text>
         </View>
